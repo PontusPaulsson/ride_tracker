d4b7a35b66e9e4decd6b0603ef25193d8f6d95d8
Pontus Paulsson <pontus.p.paulsson@gmail.com>
2018-10-31 09:30:09 +0100

    Initial Commit

--- src/test/java/com/pluralsight/controller/RestControllerTest.java
+++ src/test/java/com/pluralsight/controller/RestControllerTest.java
@@ -27,4 +27,17 @@ public class RestControllerTest {
 			System.out.println("Ride name: " + ride.getName());
 		}
 	}
+
+	@Test
+	public void testCreateRide(){
+		RestTemplate restTemplate = new RestTemplate();
+		Ride ride = new Ride();
+
+		ride.setName("Bobsledd trailride");
+		ride.setDuration(35);
+
+		restTemplate.put("http://localhost:8080/ride_tracker/ride", ride);
+
+
+	}
 }
